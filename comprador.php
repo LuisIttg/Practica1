c2cbc96ceeb1d5d50219f0397f628dc819f2a5ed
LuisIttg <luis.angel.vazquez.moreno10@gmail.com>
2020-02-10 14:29:49 -0600

    cambios2020
    este cambio lo hizo Luis

---
+++ comprador.php
@@ -0,0 +1,67 @@
+<html>
+<head>
+<title>Comprador</title>
+ <?php include 'navbar2.php'; ?>
+
+</head>
+ <body background="/images/fondo.jpg">
+<center>
+<img src="images/head.png"><br><br><br>
+<img src="images/libretas.jpg" width="250" height="254"><br><br>
+LIBRETAS ECOLÓGICAS:
+$55.00
+<br><br><br>
+
+<img src="images/lapiz.png" width="250" height="254"><br><br>
+LÁPIZ:
+$25.00 
+<br><br><br>
+
+<img src="images/lapicero.jpg" width="250" height="254"><br><br> 
+LAPICEROS:
+$40.00
+<br><br><br>
+
+<img src="images/borrador.jpg" width="250" height="254"><br><br> 
+BORRADOR:
+$5.00
+<br><br><br>
+
+<img src="images/sacapuntas.jpg" width="250" height="254"><br><br> 
+SACAPUNTAS:
+$15.00
+<br><br><br>
+
+<img src="images/postick.jpg" width="250" height="254"><br><br> 
+STICK NOTES:
+$25.00
+<br><br><br>
+
+<img src="images/plumones.jpg" width="250" height="254"><br><br> 
+PLUMONES:
+$80.00
+<br><br><br>
+
+<img src="images/corrector.jpg" width="250" height="254"><br><br> 
+CORRECTOR:
+$25.00
+<br><br><br>
+
+<img src="images/clip.jpg" width="250" height="254"><br><br>
+CLIP:
+$20.00
+<br><br><br>
+
+<img src="images/baco.jpg" width="250" height="254"><br><br>
+BACO:
+$35.00
+<br><br><br>
+
+<img src="images/plastilina.jpg" width="250" height="254"><br><br>
+PLASTILINA:
+$60.00
+</center>
+</body>
+</html>
+
+
